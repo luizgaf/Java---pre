ddced48cfe24878ab95512d091d211de5728acf0
Luiz Guilherme 0xF17A <141763490+luizgaf@users.noreply.github.com>
2026-02-12 18:55:50 -0300

    Rename quicksort to bubblesort and implement sorting

--- bubblesort.java
+++ bubblesort.java
@@ -1,35 +1,35 @@
-public class quicksort{
+public class bubblesort{
 
-        public static int[] quicksort(int[] v){
+	public static int[] bubblesort(int[] v){
 
-                int n = v.length;
+		int n = v.length;
+		System.out.println(n);
+		for(int i = 0; i < n - 1; i++){
 
-                for(int i = 0; i < n - 1; i++){
+			for(int j = 0; j < n -1 - i; j++){
 
-                        for(int j = 0; j < n -1 - i; j++){
-
-                                if(v[j] > v[j+1]){
-                                        int aux = v[j];
-                                        v[j] = v[j+1];
-                                        v[j+1] = aux;
-                                }
-                        }
-                }
+				if(v[j] > v[j+1]){
+					int aux = v[j];
+					v[j] = v[j+1];
+					v[j+1] = aux;
+				}
+			}
+		}
 
 
-                return v;
+		return v;
+		
+	}
 
-        }
+	public static void main(String[] args){
 
-        public static void main(String[] args){
+		int[] v = {3,4,2,6,1,7,4};
 
-                int[] v = {3,4,2,6,1,7,4};
+		System.out.println("Original array: " + java.util.Arrays.toString(v));
+		
+		int[] qsv = bubblesort(v);	
 
-                System.out.println("Original array: " + java.util.Arrays.toString(v));
+		System.out.println("Original array: " + java.util.Arrays.toString(qsv));
 
-                int[] qsv = quicksort(v);
-
-                System.out.println("Original array: " + java.util.Arrays.toString(qsv));
-
-        }
+	}
 }
